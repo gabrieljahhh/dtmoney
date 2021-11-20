bdaab92294d835a6378a54566c9b91137072a0e8
Gabriel Leite <galeite@gmail.com>
2021-11-20 16:50:14 -0300

    dt money

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+
+
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderPropos {
+    onOpenNewTransactionModal: () => void;
+}
+export function Header({ onOpenNewTransactionModal }: HeaderPropos) {
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type="button"
+                    onClick={onOpenNewTransactionModal}>
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
